478f725e2fbf662113641617e40f58548a313efb
Marcele <Marcele@MBUKVAR>
2016-06-28 01:24:28 -0300

    Desenvolvimmento das funcionalidades: alterar e remover (classe cadastrarDieta.java)

--- raiz/src/main/java/com/utfpr/crud/CrudDieta.java
+++ raiz/src/main/java/com/utfpr/crud/CrudDieta.java
@@ -32,7 +32,43 @@ public class CrudDieta {
             em.close();
         }
     }
-    
+    public void remove(com.utfpr.entidades.Alimentos alimento) {
+        EntityManager em = emf.createEntityManager();
+        try {
+            em.getTransaction().begin();
+            com.utfpr.entidades.Alimentos alim;
+            alim=em.find(com.utfpr.entidades.Alimentos.class, alimento.getId());
+            em.remove(alim);
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            Logger.getLogger(getClass().getName()).log(Level.SEVERE, "exception caught", e);
+            em.getTransaction().rollback();
+        } finally {
+            em.close();
+        }
+    }
+      public void update(com.utfpr.entidades.Alimentos alimento) {
+        EntityManager em = emf.createEntityManager();
+        try {
+            em.getTransaction().begin();
+            com.utfpr.entidades.Alimentos alim;
+            alim=em.find(com.utfpr.entidades.Alimentos.class, alimento.getId());
+            alim.setNome1(alimento.getNome1());
+            alim.setNome2(alimento.getNome2());
+            alim.setNome3(alimento.getNome3());
+            alim.setNome4(alimento.getNome4());
+            alim.setNome5(alimento.getNome5());
+            alim.setNome6(alimento.getNome6());
+            alim.setTipo(alimento.getTipo());
+            em.merge(alim);
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            Logger.getLogger(getClass().getName()).log(Level.SEVERE, "exception caught", e);
+            em.getTransaction().rollback();
+        } finally {
+            em.close();
+        }
+    }
     public java.util.Collection<com.utfpr.entidades.Alimentos> getAll() {
         EntityManager em = emf.createEntityManager();
         try {
